c6c22538dc656b992fa68a063e766511d14dc8b5
Nuutti Viitajylhä <nuutti.viitajylha@exove.com>
2026-02-11 20:45:59 +0200

    feat: update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,17 +1,8 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+config.color_scheme = "Catppuccin Mocha"
+config.font_size = 20
+config.window_background_opacity = 1
 
--- For example, changing the color scheme:
-config.font_size = 18
-config.window_background_opacity = 0.7
--- config.window_background_image = "~/prog/config/azskalt-little-numbat-boy.jpeg"
-
-config.color_scheme = "Catppuccin Macchiato"
-
--- and finally, return the configuration to wezterm
 return config
