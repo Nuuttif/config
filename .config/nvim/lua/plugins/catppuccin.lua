984fa55f4ec4550eecfe7aad306ad18c52d371ae
Nuutti Viitajylhä <nuutti.viitajylha@gmail.com>
2026-02-18 23:52:10 +0200

    feat: change colorscheme to rose-pine
    - Left catppuccin commented so that it's easy to go back.

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,15 +1,15 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	flavour = "macchiato",
-
-	config = function()
-		require("catppuccin").setup({
-			transparent_background = true,
-		})
-
-		-- setup must be called before loading
-		vim.cmd.colorscheme("catppuccin")
-	end,
+	-- "catppuccin/nvim",
+	-- name = "catppuccin",
+	-- priority = 1000,
+	-- flavour = "macchiato",
+	--
+	-- config = function()
+	-- 	require("catppuccin").setup({
+	-- 		transparent_background = false,
+	-- 	})
+	--
+	-- 	-- setup must be called before loading
+	-- 	vim.cmd.colorscheme("catppuccin")
+	-- end,
 }
--- .wezterm.lua
+++ .wezterm.lua
@@ -1,7 +1,8 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "rose-pine"
 config.font_size = 20
 config.window_background_opacity = 1
 
